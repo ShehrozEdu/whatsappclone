39c580b12e1f1766b395e4e7f70d6728dc43a18c
ekanshgupta981 <ekanshgurmon@gmail.com>
2022-11-17 13:54:20 +0530

    ui enhance 13:54

--- src/component/homePage/Message.js
+++ src/component/homePage/Message.js
@@ -5,25 +5,33 @@ const Message = ({ msg, user }) => {
   //   console.log(msg.createdAt);
   return (
     <>
-      {user.sub === msg.senderId ? (
-        <section className="d-flex justify-content-start">
-          <div className=" col-5 p-1 my-2 shadow border chat-green ms-5 d-flex flex-column ">
-            <div className=" chat-coming">{msg.text}</div>
-            <div className="d-flex justify-content-end text-muted ps-5 mt-0 pt-0 time-fs">
-              {FormatDate(msg.createdAt)}
+      <div className="mx-5 col-8">
+        {user.sub === msg.senderId ? (
+          <section className="d-flex justify-content-end">
+            <div className=" col-5 rounded-2 p-2 small my-1 shadow border chat-green ms-5">
+              <div className=" chat-coming">{msg.text}</div>
+              <div className="d-flex justify-content-end text-muted ps-5 mt-0 pt-0 time-fs">
+                {FormatDate(msg.createdAt)}
+              </div>
             </div>
-          </div>
-        </section>
-      ) : (
-        <section className="d-flex justify-content-end me-5">
-          <div className=" col-5 p-1 my-2 shadow border chat-green  d-flex flex-column ">
-            <div className=" chat-coming">{msg.text}</div>
-            <div className="d-flex justify-content-end text-muted ps-5 mt-0 pt-0 time-fs">
-              {FormatDate(msg.createdAt)}
+          </section>
+        ) : (
+          <section className="d-flex">
+            <div
+              className={
+                user.sub !== msg.senderId
+                  ? " col-5 rounded-2 p-2 small my-1 shadow border bg-light"
+                  : null
+              }
+            >
+              <div className=" chat-coming">{msg.text}</div>
+              <div className="d-flex justify-content-end text-muted ps-5 mt-0 pt-0 time-fs">
+                {FormatDate(msg.createdAt)}
+              </div>
             </div>
-          </div>
-        </section>
-      )}
+          </section>
+        )}
+      </div>
     </>
   );
 };
